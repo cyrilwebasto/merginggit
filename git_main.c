704c85ee5a1653ea7e7cfe7ebfc48d4f4385b03c
UdayrajPatil <udayraj.patil@external.webasto.com>
2023-07-31 16:38:52 +0530

    First commit by uday

--- git_main.c
+++ git_main.c
@@ -7,14 +7,25 @@ int calculate(int num1, int num2, int *pdiff)
 	//return num1 - num2;
 }
 
+int multiplication(int no1, int no2)
+{
+	return no1*no2;
+}
+
 int main(void)
 {
-	int n1 = 10, n2 = 20, sum, diff;
+	printf("\nUDAYRAJ branch\n");
+	int n1 = 40, n2 = 5, sum, diff,mul;
 
 	sum = calculate(n1, n2, &diff);
-
+	mul = multiplication(n1, n2);
 	printf("sum = %d\n", sum);
 	printf("diff = %d\n", diff);
+	printf("division = %d\n", mul);
+
+	printf("commit 2\n");
+	printf("commit 3\n");
+	printf("commit 4\n");
 
 	return 0;
 }
