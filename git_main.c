9b0e28b4eae4cb809217546d1d929545400ac745
Tejash jamadar <tejash.jamadar@external.webasto.com>
2023-07-31 16:46:19 +0530

    first commit

--- git_main.c
+++ git_main.c
@@ -7,14 +7,21 @@ int calculate(int num1, int num2, int *pdiff)
 	//return num1 - num2;
 }
 
+int div(int a,int b)
+{
+return a%b;
+}
+
 int main(void)
 {
-	int n1 = 10, n2 = 20, sum, diff;
+	int n1 = 10, n2 = 20, sum, diff,divide;
 
 	sum = calculate(n1, n2, &diff);
-
+    divide = div(n1,n2);
 	printf("sum = %d\n", sum);
 	printf("diff = %d\n", diff);
-
+    printf("division= %d\n",divide);
+	printf("2 commit");
+	printf("3 commit");
 	return 0;
 }
